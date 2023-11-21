292271b5220e6b9dc79ae714b2f118dbb7ca9f5a
oktapian@jkt1.ebdesk.com <oktapian@jkt1.ebdesk.com>
2023-11-21 16:55:06 +0700

    feat: :sparkles: Split metadata
    refactor basemeta, for much more dynamic needs

--- typica/base.py
+++ typica/base.py
@@ -1,9 +1,11 @@
 from uuid import uuid4
-from typing import Optional
-from pydantic import BaseModel, Field, UUID4
 from datetime import datetime
-from .utils.enums import DataStatus
+from typing import Optional
+
+from pydantic import BaseModel, Field, UUID4
 from pytz import common_timezones
+
+from .utils.enums import DataStatus
 
 
 class IdMeta(BaseModel):
@@ -22,64 +24,42 @@ class IdMongoMeta(BaseModel):
     id: str = Field(default_factory=lambda: str(uuid4()), alias="_id")
 
 
-# * Type 1 - Basic metadata
-class BaseMeta(BaseModel):
+# * Basic metadata
+class MetaCreate(BaseModel):
     """
-    Default metadata of data
+    Default create metadata
     """
 
     createdAt: Optional[int] = Field(int(datetime.now().timestamp() * 1000), ge=0)
-    updatedAt: Optional[int] = Field(None)
-    deletedAt: Optional[int] = Field(None)
     createdBy: Optional[str] = Field("")
+
+
+class MetaUpdate(BaseModel):
+    """
+    Default update metadata
+    """
+
+    updatedAt: Optional[int] = Field(None)
     updatedBy: Optional[str] = Field(None)
+
+
+class MetaDelete(BaseModel):
+    """
+    Default delete metadata
+    """
+
+    updatedAt: Optional[int] = Field(None)
     deletedBy: Optional[str] = Field(None)
+
+
+class MetaAdditional(BaseModel):
+    """
+    Default additionals metadata
+    """
+
     timezone: Optional[str] = Field("Asia/Jakarta", examples=common_timezones)
     status: Optional[DataStatus] = Field(DataStatus.active, examples=DataStatus.list())
 
     @property
     def is_active(self):
         return self.status == DataStatus.active
-
-
-class BaseIdMeta(BaseMeta, IdMeta):
-    """
-    Default metadata of data with ID
-
-    Args:
-        BaseMeta (BaseModel): Default metadata of data
-    """
-
-    ...
-
-
-class BaseIdMongoMeta(BaseMeta, IdMongoMeta):
-
-    """
-    Default metadata of data with _id in mongo
-
-    Args:
-        BaseMeta (BaseModel): Default metadata of data
-    """
-
-    ...
-
-
-# * Type 2 - Nested metadata
-class BaseNestedMeta(BaseModel):
-    """
-    Nested metadata of data
-    """
-
-    metadata: Optional[BaseMeta]
-
-
-class BaseNestedIdMeta(BaseNestedMeta, IdMeta):
-    """
-    Nested metadata of data with ID
-
-    Args:
-        BaseNestedIdMeta (BaseModel): Nested metadata of data
-    """
-
-    ...
