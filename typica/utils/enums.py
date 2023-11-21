86fdf837a48f4ba2daf3cee662ed1834970fe138
oktapian@jkt1.ebdesk.com <oktapian@jkt1.ebdesk.com>
2023-11-21 16:52:59 +0700

    feat: :sparkles: EnumV2 & EnumV3
    add description feature on upgraded enums

--- typica/utils/enums.py
+++ typica/utils/enums.py
@@ -1,30 +1,45 @@
 from enum import Enum
 
 
-class UpgradedEnum(Enum):
-    
+class EnumV2(Enum):
     @classmethod
     def list(cls):
         return list(map(lambda c: c.value, cls))
 
-class Order(str, UpgradedEnum):
-    ascending = 'ASC'
-    descending = 'DESC'
+
+class EnumV3(EnumV2):
+    def __new__(cls, *args, **kwargs):
+        obj = object.__new__(cls)
+        obj._value_ = args[0]
+        return obj
+
+    def __init__(self, value, description: str = None):
+        self._value_ = value
+        self._description_ = description
+
+    @property
+    def description(self):
+        return self._description_
 
 
-class Operator(str, UpgradedEnum):
-    equal = 'equal'
-    unequal = 'unequal'
-    regex = 'regex'
-    gte = 'gte'
-    gt = 'gt'
-    lte = 'lte'
-    lt = 'lt'
-    include = 'include'
-    exclude = 'exclude'
+class Order(EnumV3):
+    ascending = ("ASC", "sorted ascending")
+    descending = ("DESC", "sorted descending")
 
 
-class DataStatus(str, UpgradedEnum):
+class Operator(EnumV3):
+    equal = ("eq", "value is equals to")
+    unequal = ("ne", "value isn't equals to")
+    regex = ("re", "regex match")
+    gte = ("ge", "value is greater equals to")
+    gt = ("gt", "value is greater to")
+    lte = ("le", "value is lower equals to")
+    lt = ("lt", "value is lower to")
+    include = ("in", "values that must exist")
+    exclude = ("nin", "values that don't exist")
+
+
+class DataStatus(str, EnumV2):
     """
     Default status of data
     """
@@ -34,9 +49,11 @@ class DataStatus(str, UpgradedEnum):
     deleted = "deleted"
     success = "success"
     failed = "failed"
+    published = "published"
+    unpublished = "unpublished"
 
 
-class ConnectionTypes(str, UpgradedEnum):
+class ConnectionTypes(str, EnumV2):
     """
     Connection types supported on ConnectionUriMeta
     """
@@ -49,7 +66,7 @@ class ConnectionTypes(str, UpgradedEnum):
     redis = "redis"
 
 
-class ResponseMessage(str, UpgradedEnum):
+class ResponseMessage(str, EnumV2):
     """
     Base response service
     """
@@ -62,7 +79,7 @@ class ResponseMessage(str, UpgradedEnum):
     archive = "Archived"
 
 
-class ErrorResponseMessage(str, UpgradedEnum):
+class ErrorResponseMessage(str, EnumV2):
     """
     Base error response service
     """
