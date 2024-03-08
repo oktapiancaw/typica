aad342b823d1f1053c7510035a9bd9b9ca72ec2b
oktapian@jkt1.ebdesk.com <oktapian@jkt1.ebdesk.com>
2024-03-08 15:43:55 +0700

    feat: :sparkles: Mongo Query builder & mustnot query

--- typica/parameter.py
+++ typica/parameter.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, Field
 from pytz import common_timezones
 
 from .utils import Order, Operator
+from .utils.query import ChainQuery, _QueryType, BaseQuery
 
 _SearchValueType = TypeVar(
     "_SearchValueType", str, int, float, bool, list[str], list[int], None
@@ -51,3 +52,176 @@ class MultiFilterSchemas(PaginationSchemas, TimeframeSchemas, OrderSchemas):
 class BaseFilterSchemas(
     PaginationSchemas, TimeframeSchemas, OrderSchemas, SearchSchemas
 ): ...
+
+
+class MongoQueryBuilder(ChainQuery[_QueryType]):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def extract_query_must(
+        self, filters: list[SearchSchemas]
+    ) -> "MongoQueryBuilder[_QueryType]":
+        for filter_query in filters:
+            match (filter_query.opt):
+                case Operator.unequal:
+                    self.must({filter_query.field: {"$ne": filter_query.value}})
+                case Operator.include:
+                    self.must({filter_query.field: {"$in": filter_query.value}})
+                case Operator.exclude:
+                    self.must({filter_query.field: {"$nin": filter_query.value}})
+                case Operator.gt:
+                    self.must({filter_query.field: {"$gt": filter_query.value}})
+                case Operator.gte:
+                    self.must({filter_query.field: {"$gte": filter_query.value}})
+                case Operator.lt:
+                    self.must({filter_query.field: {"$lt": filter_query.value}})
+                case Operator.lte:
+                    self.must({filter_query.field: {"$lte": filter_query.value}})
+                case Operator.exist:
+                    self.must({filter_query.field: {"$exist": 1}})
+                case Operator.not_exist:
+                    self.must({filter_query.field: {"$exist": 0}})
+                case Operator.regex:
+                    self.must(
+                        {
+                            filter_query.field: {
+                                "$regex": filter_query.value,
+                                "$options": "i",
+                            }
+                        }
+                    )
+                case _:
+                    self.must({filter_query.field: filter_query.value})
+        return self
+
+    def extract_query_mustnt(
+        self, filters: list[SearchSchemas]
+    ) -> "MongoQueryBuilder[_QueryType]":
+        for filter_query in filters:
+            match (filter_query.opt):
+                case Operator.unequal:
+                    self.mustnt({filter_query.field: {"$ne": filter_query.value}})
+                case Operator.include:
+                    self.mustnt({filter_query.field: {"$in": filter_query.value}})
+                case Operator.exclude:
+                    self.mustnt({filter_query.field: {"$nin": filter_query.value}})
+                case Operator.gt:
+                    self.mustnt({filter_query.field: {"$gt": filter_query.value}})
+                case Operator.gte:
+                    self.mustnt({filter_query.field: {"$gte": filter_query.value}})
+                case Operator.lt:
+                    self.mustnt({filter_query.field: {"$lt": filter_query.value}})
+                case Operator.lte:
+                    self.mustnt({filter_query.field: {"$lte": filter_query.value}})
+                case Operator.exist:
+                    self.mustnt({filter_query.field: {"$exist": 1}})
+                case Operator.not_exist:
+                    self.mustnt({filter_query.field: {"$exist": 0}})
+                case Operator.regex:
+                    self.mustnt(
+                        {
+                            filter_query.field: {
+                                "$regex": filter_query.value,
+                                "$options": "i",
+                            }
+                        }
+                    )
+                case _:
+                    self.mustnt({filter_query.field: filter_query.value})
+        return self
+
+    def extract_query_should(
+        self, filters: list[SearchSchemas]
+    ) -> "MongoQueryBuilder[_QueryType]":
+        for filter_query in filters:
+            match (filter_query.opt):
+                case Operator.unequal:
+                    self.should({filter_query.field: {"$ne": filter_query.value}})
+                case Operator.include:
+                    self.should({filter_query.field: {"$in": filter_query.value}})
+                case Operator.exclude:
+                    self.should({filter_query.field: {"$nin": filter_query.value}})
+                case Operator.gt:
+                    self.should({filter_query.field: {"$gt": filter_query.value}})
+                case Operator.gte:
+                    self.should({filter_query.field: {"$gte": filter_query.value}})
+                case Operator.lt:
+                    self.should({filter_query.field: {"$lt": filter_query.value}})
+                case Operator.lte:
+                    self.should({filter_query.field: {"$lte": filter_query.value}})
+                case Operator.exist:
+                    self.should({filter_query.field: {"$exist": 1}})
+                case Operator.not_exist:
+                    self.should({filter_query.field: {"$exist": 0}})
+                case Operator.regex:
+                    self.should(
+                        {
+                            filter_query.field: {
+                                "$regex": filter_query.value,
+                                "$options": "i",
+                            }
+                        }
+                    )
+                case _:
+                    self.should({filter_query.field: filter_query.value})
+        return self
+
+    def extract_query_shouldnt(
+        self, filters: list[SearchSchemas]
+    ) -> "MongoQueryBuilder[_QueryType]":
+        for filter_query in filters:
+            match (filter_query.opt):
+                case Operator.unequal:
+                    self.shouldnt({filter_query.field: {"$ne": filter_query.value}})
+                case Operator.include:
+                    self.shouldnt({filter_query.field: {"$in": filter_query.value}})
+                case Operator.exclude:
+                    self.shouldnt({filter_query.field: {"$nin": filter_query.value}})
+                case Operator.gt:
+                    self.shouldnt({filter_query.field: {"$gt": filter_query.value}})
+                case Operator.gte:
+                    self.shouldnt({filter_query.field: {"$gte": filter_query.value}})
+                case Operator.lt:
+                    self.shouldnt({filter_query.field: {"$lt": filter_query.value}})
+                case Operator.lte:
+                    self.shouldnt({filter_query.field: {"$lte": filter_query.value}})
+                case Operator.exist:
+                    self.shouldnt({filter_query.field: {"$exist": 1}})
+                case Operator.not_exist:
+                    self.shouldnt({filter_query.field: {"$exist": 0}})
+                case Operator.regex:
+                    self.shouldnt(
+                        {
+                            filter_query.field: {
+                                "$regex": filter_query.value,
+                                "$options": "i",
+                            }
+                        }
+                    )
+                case _:
+                    self.shouldnt({filter_query.field: filter_query.value})
+        return self
+
+    def extract_timeframe(
+        self, timeframe: Timeframe
+    ) -> "MongoQueryBuilder[_QueryType]":
+        if timeframe.gte:
+            self.must({timeframe.field: {"$gte": timeframe.gte}})
+        if timeframe.lte:
+            self.must({timeframe.field: {"$lte": timeframe.lte}})
+        return self
+
+    @property
+    def query_mongo(self) -> dict[str, Any]:
+        query_raw = {}
+        if self.query.andOpt:
+            query_raw["$and"] = self.query.andOpt
+        if self.query.orOpt:
+            query_raw["$not"] = self.query.orOpt
+        if self.query.notOpt:
+            query_raw["$or"] = self.query.notOpt
+        if self.query.norOpt:
+            query_raw["$nor"] = self.query.norOpt
+        if self.query.other_query:
+            query_raw.update(self.query.other_query)
+        return query_raw
--- typica/utils/query.py
+++ typica/utils/query.py
@@ -3,28 +3,39 @@ from typing import Generic, TypeVar, Mapping, Optional, Any
 
 
 class BaseQuery(BaseModel):
-    andOpt: Optional[list[Mapping[str, Any]]] = Field(None, description="Must query")
-    orOpt: Optional[list[Mapping[str, Any]]] = Field(None, description="Should query")
-    norOpt: Optional[list[Mapping[str, Any]]] = Field(
-        None, description="Shouldnt query"
-    )
+    andOpt: Optional[list[Mapping[str, Any]]] = Field([], description="Must query")
+    notOpt: Optional[list[Mapping[str, Any]]] = Field([], description="Must'nt query")
+    orOpt: Optional[list[Mapping[str, Any]]] = Field([], description="Should query")
+    norOpt: Optional[list[Mapping[str, Any]]] = Field([], description="Shouldnt query")
 
     model_config = ConfigDict(extra="allow")
 
+    @property
+    def other_query(self) -> dict[str, Any]:
+        return self.model_dump(
+            by_alias=True,
+            exclude_none=True,
+            exclude={"andOpt", "notOpt", "orOpt", "norOpt"},
+        )
 
-_QueryType = TypeVar("_QueryType", bound=BaseQuery)
+
+_QueryType = TypeVar("_QueryType", bound=Mapping[str, Any])
 
 
 class ChainQuery(Generic[_QueryType]):
     def __init__(self) -> None:
-        self.query: _QueryType = BaseQuery()
+        self.query: BaseQuery = BaseQuery()
 
     def add(self, query: _QueryType) -> "ChainQuery[_QueryType]":
-        self.query.update(query)
+        self.query = BaseQuery(**self.query.model_dump(), **query)
         return self
 
     def must(self, query: _QueryType) -> "ChainQuery[_QueryType]":
         self.query.andOpt.append(query)
+        return self
+
+    def mustnt(self, query: _QueryType) -> "ChainQuery[_QueryType]":
+        self.query.notOpt.append(query)
         return self
 
     def should(self, query: _QueryType) -> "ChainQuery[_QueryType]":
@@ -37,4 +48,6 @@ class ChainQuery(Generic[_QueryType]):
 
     @property
     def query_json(self) -> dict[str, Any]:
-        return self.query.model_dump(by_alias=True, exclude_none=True)
+        return self.query.model_dump(
+            by_alias=True, exclude_none=True, exclude_defaults=True
+        )
